1f637ff79501cff11be4c2285ef302905d68da39
Tehran Bayramov <nicosspyros@gmail.com>
2025-04-18 18:15:39 +0400

    ssaas

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                hostname: "localhost",
+            },
+            {
+                hostname: "api.iberauto.az",
+            },
+        ],
+    },
 };
 
 module.exports = nextConfig;
